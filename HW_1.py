7227fd9145c4825cfd70c23ded080fd131e53e60
Tanyaevm <evm.tanya.qa@gmail.com>
2022-01-16 22:42:11 +0300

    redacted

--- HW_1.py
+++ HW_1.py
@@ -1,17 +1,52 @@
-# 1) Создать переменную типа String
-name_str = 
+#Создать переменную типа String
+name_str = 'text'
+print('name_str =', name_str, type(name_str))
 
-# 2) Создать переменную типа Integer
-# 3) Создать переменную типа Float
-# 4) Создать переменную типа Bytes
-# 5) Создать переменную типа List
-# 6) Создать переменную типа Tuple
-# 7) Создать переменную типа Set
-# 8. Создать переменную типа Frozen set
-# 9) Создать переменную типа Dict
-#
-# 10) Вывести в консоль все выше перечисленные переменные с добавлением типа данных.
-#
+#Создать переменную типа Integer
+name_int = 5
+print('name_int =', name_int, type(name_int))
+
+#Создать переменную типа Float
+name_float = 23.2
+print('name_float =', name_float, type(name_float))
+
+#Создать переменную типа Bytes
+name = 'Kate'
+name_bytes = bytes(name, 'utf-8')
+print('name_bytes = ', name_bytes, type(name_bytes))
+
+#Создать переменную типа List
+name_list = [2,4,6,7, 'dd', 7,4]
+print('name_list = ', name_list, type(name_list))
+
+#Создать переменную типа Tuple
+name_tuple = tuple('Hello world')
+print('name_tuple = ', name_tuple, type(name_tuple))
+
+#Создать переменную типа Set
+name_set = {3, 2, 'tt'}
+print('name_set = ', name_set, type(name_set))
+
+#Создать переменную типа Frozen set
+name_frozen_set = frozenset({3,2,1})
+print('name_set_frozen', name_frozen_set, type(name_frozen_set))
+
+#Создать переменную типа Dict
+name_dict = {1:4, 4:4, 7:3}
+print('name_dict = ', name_dict, type(name_dict))
+
 # 11) Создать 2 переменные String, создать переменную в которой сканкатенируете эти переменные. Вывести в консоль.
+a = 'Kate '
+b = 'Jons'
+c = a + b
+print(c)
+
 # 12) Вывести в одну строку переменные типа String и Integer используя “,” (Запятую)
+v = 'Number'
+n = 5
+print(v, n)
+
 # 13) Вывести в одну строку переменные типа String и Integer используя “+” (Плюс)
+r = 'Number '
+k = 6
+print(r + str(k))
