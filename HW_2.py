945667f923b6c94024d8c2ce60d29dea1cf1f4d2
Tanyaevm <evm.tanya.qa@gmail.com>
2022-01-16 23:56:40 +0300

    redacted

--- HW_2.py
+++ HW_2.py
@@ -39,19 +39,19 @@ print('result_m_exp = ', result_m_exp)
 # Создать переменную result_s_root в которой вы найдёте квадратный корень любой из переменной item
 result_s_root = item_1 ** 0.5
 
-#  14. Вывести result_s_root в консоль.
+# Вывести result_s_root в консоль.
 print('result_s_root = ', result_s_root)
 
-#  15. Создать переменную result_m_s_root в которой вы найдёте квадратный корень любой из переменной item используя библиотеку math.
+# Создать переменную result_m_s_root в которой вы найдёте квадратный корень любой из переменной item используя библиотеку math.
 result_m_s_root = math.sqrt(item_1)
 
-#  16. Вывести result_m_s_root в консоль.
+# Вывести result_m_s_root в консоль.
 print('result_m_s_root = ', result_m_s_root)
 
-#  17. Создать переменную result_mp_s_root в которой вы найдёте квадратный корень любой из переменной item используя библиотеку math используя метод pow.
+# Создать переменную result_mp_s_root в которой вы найдёте квадратный корень любой из переменной item используя библиотеку math используя метод pow.
 result_mp_s_root = math.pow(item_1, 0.5)
 
-#  18. Вывести result_mp_s_root в консоль.
+# Вывести result_mp_s_root в консоль.
 print('result_mp_s_root', result_mp_s_root)
 
 # #  19. Присвоить переменной item_1 odd значение
@@ -60,128 +60,129 @@ print('result_mp_s_root', result_mp_s_root)
 # #  20. Присвоить переменной item_2 even значение
 # item_2 = 'even'
 
-#  21. Создать переменную result_division в которой вы разделите item_1 на item_2.
+# Создать переменную result_division в которой вы разделите item_1 на item_2.
 result_division = item_1 / item_2
 
-#  22. Вывести result_division в консоль.
+# Вывести result_division в консоль.
 print('result_division = ', result_division)
 
-#  23. Создать переменную result_m_floor и result_division округлить до ближайшего целого меньшего чем result_division.
+# Создать переменную result_m_floor и result_division округлить до ближайшего целого меньшего чем result_division.
 result_m_floor = math.floor(result_division)
 
-#  24. Вывести result_m_floor в консоль.
+# Вывести result_m_floor в консоль.
 print('result_m_floor = ', result_m_floor)
 
-#  25. Создать переменную result_m_ceil и result_division округлить до ближайшего целого большего чем result_division.
+# Создать переменную result_m_ceil и result_division округлить до ближайшего целого большего чем result_division.
 result_m_ceil = math.ceil(result_division)
 
-#  26. Вывести result_m_ceil в консоль.
+# Вывести result_m_ceil в консоль.
 print('result_m_ceil = ', result_m_ceil)
 
-#  27. Создать переменную result_int и result_division округлить до ближайшего целого через явное приведение.
+# Создать переменную result_int и result_division округлить до ближайшего целого через явное приведение.
 result_int = round(result_division, 0)
 
-#  28. Вывести result_int в консоль.
+# Вывести result_int в консоль.
 print('result_int = ', result_int)
 
-#  29. Создать переменную result_no_division_loss в которой вы разделите item_1 на item_2 без остатка.
+# Создать переменную result_no_division_loss в которой вы разделите item_1 на item_2 без остатка.
 result_no_division = item_1 // item_2
 
-#  30. Вывести result_no_division_loss в консоль.
+# Вывести result_no_division_loss в консоль.
 print('result_no_division = ', result_no_division)
 
-#  31. Создать переменную result_division_loss в которой вы найдёте остаток от деления item_1 на item_2.
+# Создать переменную result_division_loss в которой вы найдёте остаток от деления item_1 на item_2.
 result_division_loss = item_1 % item_2
 
-#  32. Вывести result_division_loss в консоль.
+# Вывести result_division_loss в консоль.
 print('result_division_loss = ', result_division_loss)
 
 # Дальше будет реализация через арифметические действия с присваиванием.
 
 
-#  33. Создать переменную item_3 и присвоить ей целое число
+# Создать переменную item_3 и присвоить ей целое число
+item_3 = 5
 
+# Прибавить 10 к item_3 с присвоением.
+item_3 += 10
 
-#  34. Прибавить 10 к item_3 с присвоением.
+# Вывести item_3 в консоль.
+print('item_3 + 10 = ', item_3)
 
+# Отнять 5 от item_3 с присвоением.
+item_3 -= 5
 
-#  35. Вывести item_3 в консоль.
+# Вывести item_3 в консоль.
+print('item_3 - 5 =', item_3)
 
+# Умножить item_3 на 3 с присвоением.
+item_3 *= 3
 
-#  36. Отнять 5 от item_3 с присвоением.
+# Вывести item_3 в консоль.
+print('item_3 * 3 = ', item_3)
 
+# Разделить item_3 на 2 с присвоением.
+item_3 /= 2
 
-#  37. Вывести item_3 в консоль.
+# Вывести item_3 в консоль.
+print('item_3 / 2 = ', item_3)
 
+# Возвести в степень 2 item_3 с присвоением.
+item_3 **= 2
 
-#  38. Умножить item_3 на 3 с присвоением.
+# Вывести item_3 в консоль.
+print('item_3 ** 2 = ', item_3)
 
+# Найти квадратный корень item_3 с присвоением.
+item_3 **= 0.5
 
-#  39. Вывести item_3 в консоль.
+# Вывести item_3 в консоль.
+print('item_3 ** 0.5 = ', item_3)
 
+# Присвоить остаток от деления item_3
+item_3 %= 3
 
-#  40. Разделить item_3 на 2 с присвоением.
-
-
-#  41. Вывести item_3 в консоль.
-
-
-#  42. Возвести в степень 2 item_3 с присвоением.
-
-
-#  43. Вывести item_3 в консоль.
-
-
-#  44. Найти квадратный корень item_3 с присвоением.
-
-
-#  45. Вывести item_3 в консоль.
-
-
-#  46. Присвоить остаток от деления item_3
-
-
-#  47. Вывести item_3 в консоль.
+# Вывести item_3 в консоль.
+print('item_3 % 3 = ', item_3)
 
 # Boolean
 
-#  48. Создать переменную b_item_t и присвоить True
+# Создать переменную b_item_t и присвоить True
+b_item_t = True
+
+# Создать переменную b_item_f и присвоить False
+b_item_f = False
+
+# Создать переменную b_item_relult_sum и присвоить сумму b_item_t и b_item_f
+b_item_result_sum = b_item_t + b_item_f
+
+# Вывести b_item_relult_sum в консоль.
+print(b_item_result_sum)
+
+# Создать переменную b_item_relult_subtr и присвоить разницу b_item_t и b_item_f
 
 
-#  49. Создать переменную b_item_f и присвоить False
+# Вывести b_item_relult_subtr в консоль.
 
 
-#  50. Создать переменную b_item_relult_sum и присвоить сумму b_item_t и b_item_f
+# Создать переменную b_item_relult_multi и присвоить умножение b_item_t и b_item_f
 
 
-#  51. Вывести b_item_relult_sum в консоль.
+# Вывести b_item_relult_multi в консоль.
 
 
-#  52. Создать переменную b_item_relult_subtr и присвоить разницу b_item_t и b_item_f
+# Создать переменную b_item_relult_division и присвоить деление b_item_t и b_item_f
 
 
-#  53. Вывести b_item_relult_subtr в консоль.
+# Вывести b_item_relult_division в консоль. (Получить ошибку)
 
 
-#  54. Создать переменную b_item_relult_multi и присвоить умножение b_item_t и b_item_f
+# Создать переменную b_item_1_int и присвоить явное приведение b_item_t к int
 
 
-#  55. Вывести b_item_relult_multi в консоль.
+# Вывести b_item_1_int в консоль.
 
 
-#  56. Создать переменную b_item_relult_division и присвоить деление b_item_t и b_item_f
+# Создать переменную b_item_2_int и присвоить явное приведение b_item_2 к int
 
 
-#  57. Вывести b_item_relult_division в консоль. (Получить ошибку)
-
-
-#  58. Создать переменную b_item_1_int и присвоить явное приведение b_item_t к int
-
-
-#  59. Вывести b_item_1_int в консоль.
-
-
-#  60. Создать переменную b_item_2_int и присвоить явное приведение b_item_2 к int
-
-
-#  61. Вывести b_item_2_int в консоль.
+# Вывести b_item_2_int в консоль.
