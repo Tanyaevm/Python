511013d81c3dfdd4357db487a72b79799732a566
Tanyaevm <evm.tanya.qa@gmail.com>
2022-01-18 02:46:59 +0300

    new file

---
+++ currency.py
@@ -0,0 +1,75 @@
+
+
+print('Введите валюту, из которой будет перевод: usd, eur, rub, byn')
+valuta_is = input()
+print('Ваша валюта - ', valuta_is)
+
+print('Введите сумму')
+sum_val = input()
+print('Сумма = ', sum_val)
+
+print('Введите в какую валюту будет перевод: usd, eur, rub, byn')
+valuta_v = input()
+print('Итоговая валюта - ', valuta_v)
+
+usd_item = 'usd'
+eur_item = 'eur'
+rub_item = 'rub'
+byn_item = 'byn'
+
+
+#из бел.р. в другие валюты
+if valuta_is == byn_item:
+    if valuta_v == rub_item:
+        a = int(sum_val) * 29.47
+        print('rub = ', a)
+    elif valuta_v == eur_item:
+        b = int(sum_val) * 0.34
+        print('eur = ', b)
+    elif valuta_v == usd_item:
+        c = int(sum_val) * 0.39
+        print('usd = ', c)
+    elif valuta_v == byn_item:
+        d = int(sum_val) * 1
+        print('byn = ', d)
+
+#из баксов в другие валюты
+elif valuta_is == usd_item:
+    if valuta_v == rub_item:
+        a_1 = int(sum_val) * 76.19
+        print('rub = ', a_1)
+    elif valuta_v == byn_item:
+        b_1 = int(sum_val) * 2.59
+        print('byn = ', b_1)
+    elif valuta_v == eur_item:
+        c_1 = int(sum_val) * 0.88
+        print('eur = ', c_1)
+    elif valuta_v == usd_item:
+        d_1 = int(sum_val) * 1
+        print('usd = ', d_1)
+
+#из евро в другие валюты
+elif valuta_is == eur_item:
+    if valuta_v == byn_item:
+
+    if valuta_v == rub_item:
+
+    if valuta_v == usd_item:
+
+    if valuta_v == eur_item:
+        d_2 = int(sum_val) * 1
+        print('eur = ', d_2)
+
+#из рос р в другие валюты
+elif valuta_is == rub_item:
+    if valuta_v == byn_item
+    if valuta_v == usd_item
+    if valuta_v == eur_item
+    if valuta_v == rub_item:
+        d_3 = int(sum_val) * 1
+        print('rub = ', d_3)
+
+else:
+    print('Произошла ошибка')
+
+
